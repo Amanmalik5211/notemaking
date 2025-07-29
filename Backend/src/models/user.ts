17de8505174b037ef01113e0a52137af6329c20f
Prashant Awasthi <prashantawasthi251@gmail.com>
2025-07-29 14:37:18 +0530

    changed user Schema, now every note will have _id

--- Backend/src/models/user.ts
+++ Backend/src/models/user.ts
@@ -1,25 +1,33 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
+interface INote {
+  text: string;
+  _id?: string; 
+}
+
 export interface IUser extends Document {
   email: string;
   name?: string;
   DOD?: Date;
-  notes: string[];
+  notes: INote[];
   provider?: string;
   providerId?: string;
 }
+
+const NoteSchema = new Schema<INote>({
+  text: { type: String, required: true }
+});
 
 const UserSchema: Schema<IUser> = new Schema(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String },
     DOD: { type: Date },
-    notes: { type: [String], default: [] },
+    notes: { type: [NoteSchema], default: [] },
     provider: { type: String },
-    providerId: { type: String },
+    providerId: { type: String }
   },
   { timestamps: true }
 );
 
 export default mongoose.model<IUser>('User', UserSchema);
-
