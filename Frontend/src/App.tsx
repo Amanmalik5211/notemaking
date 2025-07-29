0cf6b7e7dc3197888e6dfa523db65ab16225e850
prashant awasthi <99684646+prash459865@users.noreply.github.com>
2025-07-29 22:19:33 +0530

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import Login from './Pages/LoginAndSignUpPage/Login';
 import Signup from './Pages/LoginAndSignUpPage/Signup';
 import Dashboard from './Pages/DashBoard/Dashboard';
 import ProtectedUI from './Components/ProtectedUI';
 
 
+
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/dashboard" element={<ProtectedUI><Dashboard /></ProtectedUI>} />
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-       <Route path="/dashboard" element={<ProtectedUI><Dashboard /></ProtectedUI>} />
       </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </Router>
   )
 }
