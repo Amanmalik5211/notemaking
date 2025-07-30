81e2eadc1d4e147989dfe0a60ed0b9423501c587
prashant awasthi <99684646+prash459865@users.noreply.github.com>
2025-07-30 08:47:34 +0530

    Add files via upload

---
+++ Backend/dist/routes/authRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getOTP, verifyOtpforSignup, uiValidation, verifyOtpforLogin, logout, googleLogin } from "../controllers/authController.js";
+import { protectedRoute } from "../middlewares/authMiddleware.js";
+const router = express.Router();
+router.post('/get-otp', getOTP);
+router.post('/verify-otp-for-signup', verifyOtpforSignup);
+router.post('/verify-otp-for-login', verifyOtpforLogin);
+router.get('/uiValidation', uiValidation);
+router.get('/logout', protectedRoute, logout);
+router.post('/google-login', googleLogin);
+export default router;
---
+++ Backend/dist/routes/noteRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { protectedRoute } from '../middlewares/authMiddleware.js';
+import { addNote, fetchDetails, deleteNote } from '../controllers/noteController.js';
+const router = express.Router();
+router.get('/fetch-details', protectedRoute, fetchDetails);
+router.post('/add-note', protectedRoute, addNote);
+router.post('/delete-note', protectedRoute, deleteNote);
+export default router;
